30d87824458a1ca946c835c091891cd591819e28
IHMHR <martinelli.igor@hotmail.com>
2017-03-03 00:59:53 -0300

    Adição do meu nome ao final

--- sample.c
+++ sample.c
@@ -2,8 +2,10 @@
 
 int main()
 {
+	char nome[20] = "Igor Martinelli";
     printf("Hello World !\n");
     printf("Now I'm' about to commit and push this changes.\n");
+    printf("\n\n\n\nCreated by %s\n", nome);
     system("pause");
     return 0;
 }
