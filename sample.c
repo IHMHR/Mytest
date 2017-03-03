7cb6a408c810290a83d50ebbd767d31eb36e251a
IHMHR <martinelli.igor@hotmail.com>
2017-03-03 00:57:37 -0300

    Commit incorreto

--- sample.c
+++ sample.c
@@ -3,7 +3,6 @@
 int main()
 {
     printf("Hello World !\n");
-    printf("I just clone my own repo !\n");
     printf("Now I'm' about to commit and push this changes.\n");
     system("pause");
     return 0;
